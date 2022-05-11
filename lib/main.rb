20c5d486ba8733bf1a0b2f66927c1caf13347948
Kai Stevens <kai.stevens97@gmail.com>
2022-05-11 12:03:00 +0100

    outlined steps for serialization

--- lib/main.rb
+++ lib/main.rb
@@ -1,7 +1,6 @@
 require_relative 'Player'
 require_relative 'Game'
 require_relative 'Gameplay'
-require 'json'
 
 include Gameplay
 
@@ -19,10 +18,9 @@ my_game = Game.new(john, secret_word = get_word(dictionary))
 puts "The player of this game: #{my_game.player.name}"
 puts "The secret word of this game: #{my_game.secret_word}"
 
-# Serialisation test
-
-my_game_saved = my_game.to_json
-
-puts my_game_saved
-
-#my_game.game_loop()
+# Serialization to-do
+# - create a way for player to save progress during a turn
+# - so this needs to happen during the game_loop method
+# - output that save to file stored inside a saves folder (thinking individual save files for each game instance)
+# - will do it with yaml initially
+# - at the start, a player should be able to retrieve their save and play on from there
